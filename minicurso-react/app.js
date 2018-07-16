85813719c66608b411c943b1e013f1413b6e36ba
Unknown <juasmartinezbel@unal.edu.co>
2018-07-16 16:43:20 -0500

    Listas

--- minicurso-react/app.js
+++ minicurso-react/app.js
@@ -4,46 +4,65 @@
 */
 
 
-//JS6:
-
-// General:
-// Se usan parametros por elemento
-const User = props =>{
-	return (
-		<div>
-			<img src={props.img} alt={props.name} />
-			<p>{props.name}</p>
-		</div>
-
-	)
-}
-
-
 // Dividido:
 // Se usan objetos al tener intermedios [User2] entre el render y el componente [Avatar y Username]
 const Avatar = props => <img src={props.user.img} alt={props.user.name}/>;
 
 const UserName = props => <p>{props.user.name}</p>
 
-const User2 = props =>{
+const User = props =>{
 	return (
 		<div className="user-item">
 			<Avatar user={props.user}/>
 			<UserName user={props.user}/>
 		</div>
-
 	)
 };
 
-const new_user = {
-	name: "Miguel Charry",
-	img: "https://lh4.googleusercontent.com/W6JNTPUM294-SeTm0n7T6YZoaeja0EDI__Mw_eMvOrUvKBOOrNSM1C6WAF3uUI2qAK2LrFeyHiCu2bI=w1301-h651"
+const users = [
+	{
+		name: "Alejandra Drispe",
+		img: "https://lh5.googleusercontent.com/UXNZ8cZ4gd2Uh5-N3I_9lfmqehQAVwHIba6DqNsMXbL6gkT4iqprntSqLtvIX_5Hlw8jr-W53ZQNtAg=w1301-h651"
+	},
+
+	{
+		name: "Miguel Charry",
+		img: "https://lh4.googleusercontent.com/W6JNTPUM294-SeTm0n7T6YZoaeja0EDI__Mw_eMvOrUvKBOOrNSM1C6WAF3uUI2qAK2LrFeyHiCu2bI=w1301-h651"
+	},
+	{
+		name: "Rafael",
+		img: "https://lh6.googleusercontent.com/8lRFb3RZEPQ3vACEhnUH5hDOMLu1CKuNEbO1eLbZDifrFVNxuUn2Ej393SinKaLIVo2ypxLfLvq4LgM=w1301-h651"
+	}
+]
+
+
+const UsersList = props =>{
+						//list es la lista que se pasa en render
+	const userList = props.list.map(
+			(u,i) => // Cada elemento de la lista - El Index del Arreglo
+			<User user = {u} key = {i} />
+	);
+
+	return (
+			<div className = "user-list">
+				{userList}
+			</div>
+		);
 }
-//Se pasará name como un elemento de los props
+
+// Se puede hacer uno por uno ciclo o por mapeo. ¿Pero ciclo es conveniente?
 ReactDOM.render(
 		<div>
-			<User name = "Alejandra Drispe"img="https://lh5.googleusercontent.com/UXNZ8cZ4gd2Uh5-N3I_9lfmqehQAVwHIba6DqNsMXbL6gkT4iqprntSqLtvIX_5Hlw8jr-W53ZQNtAg=w1301-h651" />
-			<User2 user = {new_user} />
+			<div className="unoPorUno">
+				<User user = {users[0]} />
+				<User user = {users[1]} />
+				<User user = {users[2]} />
+			</div>
+			<div className="mapa">
+			------------------------------
+			<br/>
+				<UsersList list={users} />
+			</div>
 		</div>
 	, document.getElementById('app')
 	);
--- minicurso-react/dist-app.js
+++ minicurso-react/dist-app.js
@@ -3,23 +3,8 @@
 
 */
 
-//JS6:
-
-// General: 
-const User = props => {
-	return React.createElement(
-		"div",
-		null,
-		React.createElement("img", { src: props.img, alt: props.name }),
-		React.createElement(
-			"p",
-			null,
-			props.name
-		)
-	);
-};
-
-// Dividido
+// Dividido:
+// Se usan objetos al tener intermedios [User2] entre el render y el componente [Avatar y Username]
 const Avatar = props => React.createElement("img", { src: props.user.img, alt: props.user.name });
 
 const UserName = props => React.createElement(
@@ -28,7 +13,7 @@ const UserName = props => React.createElement(
 	props.user.name
 );
 
-const User2 = props => {
+const User = props => {
 	return React.createElement(
 		"div",
 		{ className: "user-item" },
@@ -37,13 +22,45 @@ const User2 = props => {
 	);
 };
 
-const new_user = {
+const users = [{
+	name: "Alejandra Drispe",
+	img: "https://lh5.googleusercontent.com/UXNZ8cZ4gd2Uh5-N3I_9lfmqehQAVwHIba6DqNsMXbL6gkT4iqprntSqLtvIX_5Hlw8jr-W53ZQNtAg=w1301-h651"
+}, {
 	name: "Miguel Charry",
 	img: "https://lh4.googleusercontent.com/W6JNTPUM294-SeTm0n7T6YZoaeja0EDI__Mw_eMvOrUvKBOOrNSM1C6WAF3uUI2qAK2LrFeyHiCu2bI=w1301-h651"
-	//Se pasará name como un elemento de los props
-};ReactDOM.render(React.createElement(
+}, {
+	name: "Rafael",
+	img: "https://lh6.googleusercontent.com/8lRFb3RZEPQ3vACEhnUH5hDOMLu1CKuNEbO1eLbZDifrFVNxuUn2Ej393SinKaLIVo2ypxLfLvq4LgM=w1301-h651"
+}];
+
+const UsersList = props => {
+	//list es la lista que se pasa en render
+	const userList = props.list.map((u, i) => // Cada elemento de la lista - El Index del Arreglo
+	React.createElement(User, { user: u, key: i }));
+
+	return React.createElement(
+		"div",
+		{ className: "user-list" },
+		userList
+	);
+};
+
+// Se puede hacer uno por uno ciclo o por mapeo
+ReactDOM.render(React.createElement(
 	"div",
 	null,
-	React.createElement(User, { name: "Alejandra Drispe", img: "https://lh5.googleusercontent.com/UXNZ8cZ4gd2Uh5-N3I_9lfmqehQAVwHIba6DqNsMXbL6gkT4iqprntSqLtvIX_5Hlw8jr-W53ZQNtAg=w1301-h651" }),
-	React.createElement(User2, { user: new_user })
+	React.createElement(
+		"div",
+		{ className: "unoPorUno" },
+		React.createElement(User, { user: users[0] }),
+		React.createElement(User, { user: users[1] }),
+		React.createElement(User, { user: users[2] })
+	),
+	React.createElement(
+		"div",
+		{ className: "mapa" },
+		"------------------------------",
+		React.createElement("br", null),
+		React.createElement(UsersList, { list: users })
+	)
 ), document.getElementById('app'));
