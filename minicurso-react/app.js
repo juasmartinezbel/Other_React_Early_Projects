8b8f79336b1cf8292503c7109e6286b53ddbeb56
Unknown <juasmartinezbel@unal.edu.co>
2018-07-16 16:07:49 -0500

    Props pequeños en una función JS6. General y Dividido

--- minicurso-react/app.js
+++ minicurso-react/app.js
@@ -3,21 +3,47 @@
 
 */
 
-//JS5: 
-function Saludar(props){
-	//Props es un objeto
-	return (<h1> Hola {props.name} </h1>);
+
+//JS6:
+
+// General:
+// Se usan parametros por elemento
+const User = props =>{
+	return (
+		<div>
+			<img src={props.img} alt={props.name} />
+			<p>{props.name}</p>
+		</div>
+
+	)
 }
 
+
+// Dividido:
+// Se usan objetos al tener intermedios [User2] entre el render y el componente [Avatar y Username]
+const Avatar = props => <img src={props.user.img} alt={props.user.name}/>;
+
+const UserName = props => <p>{props.user.name}</p>
+
+const User2 = props =>{
+	return (
+		<div className="user-item">
+			<Avatar user={props.user}/>
+			<UserName user={props.user}/>
+		</div>
+
+	)
+};
+
+const new_user = {
+	name: "Miguel Charry",
+	img: "https://lh4.googleusercontent.com/W6JNTPUM294-SeTm0n7T6YZoaeja0EDI__Mw_eMvOrUvKBOOrNSM1C6WAF3uUI2qAK2LrFeyHiCu2bI=w1301-h651"
+}
 //Se pasará name como un elemento de los props
 ReactDOM.render(
 		<div>
-			<Saludar name = "Alejandra"/>
-			<Saludar name = "Miguel"/>
-			<Saludar name = "Rafael"/>
-			<Saludar name = "Sara"/>
-			<Saludar name = "Agata"/>
-			<Saludar name = "Gabriel"/>
+			<User name = "Alejandra Drispe"img="https://lh5.googleusercontent.com/UXNZ8cZ4gd2Uh5-N3I_9lfmqehQAVwHIba6DqNsMXbL6gkT4iqprntSqLtvIX_5Hlw8jr-W53ZQNtAg=w1301-h651" />
+			<User2 user = {new_user} />
 		</div>
 	, document.getElementById('app')
 	);
--- minicurso-react/dist-app.js
+++ minicurso-react/dist-app.js
@@ -3,25 +3,47 @@
 
 */
 
-function Saludar(props) {
-	//Props es un objeto
-	return React.createElement(
-		"h1",
-		null,
-		" Hola ",
-		props.name,
-		" "
-	);
-}
+//JS6:
 
-//Se pasará name como un elemento de los props
-ReactDOM.render(React.createElement(
+// General: 
+const User = props => {
+	return React.createElement(
+		"div",
+		null,
+		React.createElement("img", { src: props.img, alt: props.name }),
+		React.createElement(
+			"p",
+			null,
+			props.name
+		)
+	);
+};
+
+// Dividido
+const Avatar = props => React.createElement("img", { src: props.user.img, alt: props.user.name });
+
+const UserName = props => React.createElement(
+	"p",
+	null,
+	props.user.name
+);
+
+const User2 = props => {
+	return React.createElement(
+		"div",
+		{ className: "user-item" },
+		React.createElement(Avatar, { user: props.user }),
+		React.createElement(UserName, { user: props.user })
+	);
+};
+
+const new_user = {
+	name: "Miguel Charry",
+	img: "https://lh4.googleusercontent.com/W6JNTPUM294-SeTm0n7T6YZoaeja0EDI__Mw_eMvOrUvKBOOrNSM1C6WAF3uUI2qAK2LrFeyHiCu2bI=w1301-h651"
+	//Se pasará name como un elemento de los props
+};ReactDOM.render(React.createElement(
 	"div",
 	null,
-	React.createElement(Saludar, { name: "Alejandra" }),
-	React.createElement(Saludar, { name: "Miguel" }),
-	React.createElement(Saludar, { name: "Rafael" }),
-	React.createElement(Saludar, { name: "Sara" }),
-	React.createElement(Saludar, { name: "Agata" }),
-	React.createElement(Saludar, { name: "Gabriel" })
+	React.createElement(User, { name: "Alejandra Drispe", img: "https://lh5.googleusercontent.com/UXNZ8cZ4gd2Uh5-N3I_9lfmqehQAVwHIba6DqNsMXbL6gkT4iqprntSqLtvIX_5Hlw8jr-W53ZQNtAg=w1301-h651" }),
+	React.createElement(User2, { user: new_user })
 ), document.getElementById('app'));
