3dc203c09deef2ecfa724f2eadd009d4a0b7d182
Unknown <juasmartinezbel@unal.edu.co>
2018-07-15 19:15:35 -0500

    Nuevo componente creado

--- projectmanager/src/App.js
+++ projectmanager/src/App.js
@@ -1,21 +1,23 @@
+//Dependencias:
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+//Hojas de Estilos
 import './App.css';
+
+//Components
+import Projects from './Components/Projects' //No se necesita el .js
+
+//Solo puede haber un main <div className="Archivo">
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        My App
+        <Projects />
       </div>
     );
   }
 }
 
-export default App;
+export default App; // Retorna el JSX como un componente o toda la ppagina a mostrar
---
+++ projectmanager/src/Components/Projects.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//NO necesita su propio css. Ese puede ser el general de APP
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="Projects">
+        My Projects
+      </div>
+    );
+  }
+}
+
+export default Projects;
+
+/*
+* Muestra todo lo relacionado a mostrar componentes, en este caso, retorna el componente de proyectos
+*/
