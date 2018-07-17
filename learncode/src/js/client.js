427a126eb49c00a98917c463e443c5e23f5c23f1
Unknown <juasmartinezbel@unal.edu.co>
2018-07-17 14:46:04 -0500

    Cacharreo para llamar funciones

--- learncode/src/js/client.js
+++ learncode/src/js/client.js
@@ -2,8 +2,27 @@ import React from "react"
 import ReactDOM from "react-dom";
 
 class Layout extends React.Component{
+	constructor(){
+		super();
+		this.character = "Alejandra Drispe"
+	}
+
+	getVal(){
+		return "Toto - Africa";
+	}
+
 	render(){
-		return (<h1>It's working!</h1>);
+		const name = "Zebitas"
+		return (
+			<div>
+				<h1>It's ALIVE</h1><br/>
+				<h1>{3+2}</h1><br/>
+				<h1>{name}</h1><br/>
+				<h1>{this.character}</h1><br/>
+				<h1>{(function(){return 3;})()}</h1>
+				<h1>{this.getVal()}</h1><br/>
+			</div>
+			);
 	}
 }
 
