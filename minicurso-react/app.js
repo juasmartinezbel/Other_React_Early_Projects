78723f3ff55ca402d0d4f4a4694bbd66e32570d1
Unknown <juasmartinezbel@unal.edu.co>
2018-07-16 21:07:20 -0500

    Aplicación cambia de estado para saludar y despedirse

--- minicurso-react/app.js
+++ minicurso-react/app.js
@@ -1,68 +1,39 @@
-/*
-./node_modules/babel-cli/bin/babel.js --plugins transform-react-jsx app.js --out-file dist-app.js
-
-*/
+//¿Qué hacemos para que un componente se cambie?
+// ¡CAMBIAR EL ESTADO!
 
 
-// Dividido:
-// Se usan objetos al tener intermedios [User2] entre el render y el componente [Avatar y Username]
-const Avatar = props => <img src={props.user.img} alt={props.user.name}/>;
+//Se necesitarán clases para cambiar el estado de las cosas
+class HelloClass extends React.Component{
+	constructor(props){
+		super(props);
 
-const UserName = props => <p>{props.user.name}</p>
-
-const User = props =>{
-	return (
-		<div className="user-item">
-			<Avatar user={props.user}/>
-			<UserName user={props.user}/>
-		</div>
-	)
-};
-
-const users = [
-	{
-		name: "Alejandra Drispe",
-		img: "https://lh5.googleusercontent.com/UXNZ8cZ4gd2Uh5-N3I_9lfmqehQAVwHIba6DqNsMXbL6gkT4iqprntSqLtvIX_5Hlw8jr-W53ZQNtAg=w1301-h651"
-	},
-
-	{
-		name: "Miguel Charry",
-		img: "https://lh4.googleusercontent.com/W6JNTPUM294-SeTm0n7T6YZoaeja0EDI__Mw_eMvOrUvKBOOrNSM1C6WAF3uUI2qAK2LrFeyHiCu2bI=w1301-h651"
-	},
-	{
-		name: "Rafael",
-		img: "https://lh6.googleusercontent.com/8lRFb3RZEPQ3vACEhnUH5hDOMLu1CKuNEbO1eLbZDifrFVNxuUn2Ej393SinKaLIVo2ypxLfLvq4LgM=w1301-h651"
+		//El estado será un objeto, el cual se declara y se le ponen los atributos
+		//Los estados van a cambiar por estado
+		this.state = {
+			nombre: 'Alvaro',
+			saludo: 'Buenos Días'
+		}
+		this.despedirse = this.despedirse.bind(this);
 	}
-]
 
+	//Se dispara al despedirse
+	despedirse(){
+		alert('adios');
+		//this.state.saludo = 'Hasta Luego' <-- MAL, no se puede hacer directamente
+		this.setState({
+			saludo: 'Hasta Luego'
+		})
+	};
 
-const UsersList = props =>{
-						//list es la lista que se pasa en render
-	const userList = props.list.map(
-			(u,i) => // Cada elemento de la lista - El Index del Arreglo
-			<User user = {u} key = {i} />
-	);
-
-	return (
-			<div className = "user-list">
-				{userList}
-			</div>
+	render(){
+		//Ya que estamos en una clase, se necesita poner this.
+		//Las acciones que afectarán el estado se escriben en el html, en este caso click a despedirse.
+		return(
+			//<h1>Hola {this.props.nombre}</h1>
+			<h1 onClick={this.despedirse}>{this.state.saludo} {this.state.nombre}</h1>
 		);
+	}
 }
 
-// Se puede hacer uno por uno ciclo o por mapeo. ¿Pero ciclo es conveniente?
-ReactDOM.render(
-		<div>
-			<div className="unoPorUno">
-				<User user = {users[0]} />
-				<User user = {users[1]} />
-				<User user = {users[2]} />
-			</div>
-			<div className="mapa">
-			------------------------------
-			<br/>
-				<UsersList list={users} />
-			</div>
-		</div>
-	, document.getElementById('app')
-	);
+
+ReactDOM.render(<HelloClass />, document.getElementById('app'))
--- minicurso-react/dist-app.js
+++ minicurso-react/dist-app.js
@@ -1,66 +1,46 @@
-/*
-./node_modules/babel-cli/bin/babel.js --plugins transform-react-jsx app.js --out-file dist-app.js
+//¿Qué hacemos para que un componente se cambie?
+// ¡CAMBIAR EL ESTADO!
 
-*/
 
-// Dividido:
-// Se usan objetos al tener intermedios [User2] entre el render y el componente [Avatar y Username]
-const Avatar = props => React.createElement("img", { src: props.user.img, alt: props.user.name });
+//Se necesitarán clases para cambiar el estado de las cosas
+class HelloClass extends React.Component {
+	constructor(props) {
+		super(props);
 
-const UserName = props => React.createElement(
-	"p",
-	null,
-	props.user.name
-);
+		//El estado será un objeto, el cual se declara y se le ponen los atributos
+		//Los estados van a cambiar por estado
+		this.state = {
+			nombre: 'Alvaro',
+			saludo: 'Buenos Días'
+		};
+		this.despedirse = this.despedirse.bind(this);
+	}
 
-const User = props => {
-	return React.createElement(
-		"div",
-		{ className: "user-item" },
-		React.createElement(Avatar, { user: props.user }),
-		React.createElement(UserName, { user: props.user })
-	);
-};
+	//Se dispara al despedirse.
+	//Colocar despedirse(){} no cambiará el estado. Lo mejor es ponerlo como dice a continuación:
 
-const users = [{
-	name: "Alejandra Drispe",
-	img: "https://lh5.googleusercontent.com/UXNZ8cZ4gd2Uh5-N3I_9lfmqehQAVwHIba6DqNsMXbL6gkT4iqprntSqLtvIX_5Hlw8jr-W53ZQNtAg=w1301-h651"
-}, {
-	name: "Miguel Charry",
-	img: "https://lh4.googleusercontent.com/W6JNTPUM294-SeTm0n7T6YZoaeja0EDI__Mw_eMvOrUvKBOOrNSM1C6WAF3uUI2qAK2LrFeyHiCu2bI=w1301-h651"
-}, {
-	name: "Rafael",
-	img: "https://lh6.googleusercontent.com/8lRFb3RZEPQ3vACEhnUH5hDOMLu1CKuNEbO1eLbZDifrFVNxuUn2Ej393SinKaLIVo2ypxLfLvq4LgM=w1301-h651"
-}];
+	despedirse() {
+		alert('adios');
+		//this.state.saludo = 'Hasta Luego' <-- MAL, no se puede hacer directamente
+		this.setState({
+			saludo: 'Hasta Luego'
+		});
+	}
 
-const UsersList = props => {
-	//list es la lista que se pasa en render
-	const userList = props.list.map((u, i) => // Cada elemento de la lista - El Index del Arreglo
-	React.createElement(User, { user: u, key: i }));
+	render() {
+		//Ya que estamos en una clase, se necesita poner this.
+		//Las acciones que afectarán el estado se escriben en el html, en este caso click a despedirse.
+		return (
+			//<h1>Hola {this.props.nombre}</h1>
+			React.createElement(
+				'h1',
+				{ onClick: this.despedirse },
+				this.state.saludo,
+				' ',
+				this.state.nombre
+			)
+		);
+	}
+}
 
-	return React.createElement(
-		"div",
-		{ className: "user-list" },
-		userList
-	);
-};
-
-// Se puede hacer uno por uno ciclo o por mapeo
-ReactDOM.render(React.createElement(
-	"div",
-	null,
-	React.createElement(
-		"div",
-		{ className: "unoPorUno" },
-		React.createElement(User, { user: users[0] }),
-		React.createElement(User, { user: users[1] }),
-		React.createElement(User, { user: users[2] })
-	),
-	React.createElement(
-		"div",
-		{ className: "mapa" },
-		"------------------------------",
-		React.createElement("br", null),
-		React.createElement(UsersList, { list: users })
-	)
-), document.getElementById('app'));
+ReactDOM.render(React.createElement(HelloClass, null), document.getElementById('app'));
