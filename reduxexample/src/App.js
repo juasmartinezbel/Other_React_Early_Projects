0a73cd0e7277527d6eac9c620cb50fa12f1798e5
Unknown <juasmartinezbel@unal.edu.co>
2018-07-21 18:06:24 -0500

    Convierte los datos recibidos a parte del V

--- reduxexample/src/App.js
+++ reduxexample/src/App.js
@@ -12,14 +12,6 @@
 //Todo se tomará de aquí: https://jsonplaceholder.typicode.com/
 class App extends Component {
 
-  //Cuando el componente se monte, ejecutará la acción
-  componentWillMount(){
-    //console.log(123);
-    fetch('https://jsonplaceholder.typicode.com/posts') //Hacemos fetch del origen y retorna una promesa
-    .then(res => res.json()) //El resultado res se debe pasar a un json
-    .then(data => console.log(data)) //El resultado data del anterior se pasa a los logs
-  }
-
   render() {
     return (
       <div className="App">
--- reduxexample/src/components/Posts.js
+++ reduxexample/src/components/Posts.js
@@ -1,10 +1,34 @@
 import React from 'react'
 
 class Posts extends React.Component{
+	constructor (props){
+    	super(props);
+    	this.state = {
+      		posts: []
+    	}
+  	}
+
+
+  	//Cuando el componente se monte, ejecutará la acción
+  	componentWillMount(){
+    	//console.log(123);
+		fetch('https://jsonplaceholder.typicode.com/posts') //Hacemos fetch del origen y retorna una promesa
+    	.then(res => res.json()) //El resultado res se debe pasar a un json
+    	.then(data => this.setState({posts:data}) ) //El resultado data del anterior se pasa al estado
+  	}
 	render(){
+
+		const postItems = this.state.posts.map(posts=>(
+        	<div key={posts.id}>
+          	<h3>{posts.title}</h3>
+          	<p>{posts.body}</p>
+        	</div>
+      	));
+
 		return (
 			<div>
 				<h1>Posts</h1>
+				{postItems}
 			</div>
 		)
 	}
