97b70f93e7b42034c5944a9f099347cb317b3e2e
Unknown <juasmartinezbel@unal.edu.co>
2018-07-20 23:30:08 -0500

    React-Redux

--- learncode/src/js/client.js
+++ learncode/src/js/client.js
@@ -1,47 +1,17 @@
-//Dependencies
-	import React from "react"
-	import ReactDOM from "react-dom";
-	import {applyMiddleware, createStore} from "redux";
-	import {createLogger} from "redux-logger"; //npm install -S "redux-logger"
-	import thunk from "redux-thunk"; //npm install -S "redux-thunk"
-	import axios from "axios"; //npm install -S "axios"
-	import promise from "redux-promise-middleware"
-const initialState={
-	fetching: false,
-	fetched: false,
-	users: [],
-	error: null
-};
+import React from "react"
+import ReactDOM from "react-dom"
+import {Provider} from "react-redux"
+//Hay que instalar npm install -S react-redux
 
-const reducer = function(state=initialState, action){
-	switch(action.type){
-		case "FETCH_USER_PENDING":{
-			return {...state, fetching: true}
-			break;
-		}
-		case "FETCH_USER_REJECTED":{
-			return {...state, fetching: false, error: action.payload}
-			break;
-		}
-		case "FETCH_USER_FULFILLED" :{
-			return {...state, fetching: false, fetched: true, users: action.payload}
-			break;
-		}
-	}
-  	return state 
-}
+import Layout from "./components/Layout"
+import store from "./store"
 
-							
-const middleware = applyMiddleware(promise(), thunk, createLogger());
+const app = document.getElementById('app')
 
-const store = createStore(reducer, middleware)
 
-//
-store.dispatch({
-	type:"FETCH_USER",
-	payload: axios.get("http://rest.learncode.academy/api/wstern/users") //Es un tipo promesa y enviará el tipo para guardarse
-	//Si es valido será TYPE_PENDING ---> TYPE_FULFILLED
-	//Si tiene errores será TYPE_PENDING--->TYPE_REJECTED
-})
-
-//Es un montón de funciones sincronas
+//Siempre hay que renderizar el proveedor más bien con un store asignado
+ReactDOM.render(
+		<Provider store={store}>
+			<Layout/>
+		</Provider>
+			, app);
--- learncode/src/js/components/Layout.js
+++ learncode/src/js/components/Layout.js
@@ -1,26 +1,35 @@
 import React from "react";
+import {connect} from "react-redux"
+import {fetchUser} from "../actions/userActions"
+import {fetchTweets} from "../actions/tweetsActions"
+//Hay que poner wrappear componentes del store
+@connect((store) => {
+	return{
+		//La variable toman su nombre de index.js
+		user: store.user.user,
+		userFetched: store.user.fetched,
+		tweets: store.tweets.tweets
+	};
 
-import Footer from "./Footer";
-import Header from "./Header";
-
+})
+//store values como props, 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      title: "Welcome",
-    };
-  }
+	componentWillMount(){
+		//Traer una acción
+		this.props.dispatch(fetchUser())
+	}
 
-  changeTitle(title) {
-    this.setState({title});
-  }
+	fetchTweets(){
+		this.props.dispatch(fetchTweets())
+	}
 
-  render() {
-    return (
-      <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
-        <Footer />
-      </div>
-    );
-  }
+	render(){
+		console.log(this.props)
+		const {user, tweets} = this.props;
+
+		if(!tweets.length){
+			return <button onClick={this.fetchTweets.bind(this)}>load tweets</button>
+		}
+		return <h1>{this.props.user.name}</h1>;
+	}
 }
