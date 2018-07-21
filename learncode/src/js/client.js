7965eea1588907bf2d44cde15272e1b477874a12
Unknown <juasmartinezbel@unal.edu.co>
2018-07-20 19:25:30 -0500

    Inicializando Redux y cambios por consola

--- learncode/src/js/client.js
+++ learncode/src/js/client.js
@@ -1,10 +1,32 @@
 //Dependencies
 	import React from "react"
 	import ReactDOM from "react-dom";
-
-//Components
-	import Layout from "./components/Layout"
+	import {createStore} from "redux";
 
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+const reducer = function(state, action){
+	//return "foo" //Retornará siempre esto
+
+	if(action.type == "INC"){
+		return state+action.payload;
+	}else if(action.type == "DEC"){
+		return state-action.payload;
+	}
+
+	return state
+
+}
+								// Estado inicial
+const store = createStore(reducer, 0)
+
+//Store listo, ya nos podemos subscribir
+
+store.subscribe (()=>{
+	console.log("store changed", store.getState())
+})
+
+store.dispatch({type: "INC", payload: 1})
+store.dispatch({type: "INC", payload: 1})
+store.dispatch({type: "INC", payload: 1})
+store.dispatch({type: "INC", payload: 2})//Habrá un mensaje por dispatch
+store.dispatch({type: "DEC", payload: 3})
