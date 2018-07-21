74543b0051668120bcfe92e708b1ddf32d5152ee
Unknown <juasmartinezbel@unal.edu.co>
2018-07-20 20:09:17 -0500

    Reducers con Objetos. Mezclar Reducers

--- learncode/src/js/client.js
+++ learncode/src/js/client.js
@@ -1,32 +1,41 @@
 //Dependencies
 	import React from "react"
 	import ReactDOM from "react-dom";
-	import {createStore} from "redux";
+	import {combineReducers, createStore} from "redux";
 
 
-const reducer = function(state, action){
-	//return "foo" //Retornará siempre esto
-
-	if(action.type == "INC"){
-		return state+action.payload;
-	}else if(action.type == "DEC"){
-		return state-action.payload;
+const userReducer = (state={}, action) =>{
+	switch(action.type){
+		case "CHANGE_NAME":{
+					//clonar para cambiar un pedazo del archivo.
+			state = {...state, name: action.payload}
+			break;
+		}
+		case "CHANGE_AGE":{
+			state = {...state, age: action.payload}
+			break;
+		}
 	}
+	return state //default, el vacío en argumento arriba. Un objeto vacío tipo user
+};
 
-	return state
 
+const tweetsReducer = (state = [], action) =>{
+	return state //default, el vacío en argumento arriba. Un objeto vacío tipo user		
 }
-								// Estado inicial
-const store = createStore(reducer, 0)
+
+const reducers = combineReducers({
+	user: userReducer,
+	tweets: tweetsReducer
+})
+								
+const store = createStore(reducers); 
 
 //Store listo, ya nos podemos subscribir
-
 store.subscribe (()=>{
 	console.log("store changed", store.getState())
 })
 
-store.dispatch({type: "INC", payload: 1})
-store.dispatch({type: "INC", payload: 1})
-store.dispatch({type: "INC", payload: 1})
-store.dispatch({type: "INC", payload: 2})//Habrá un mensaje por dispatch
-store.dispatch({type: "DEC", payload: 3})
+store.dispatch({type: "CHANGE_NAME", payload: "Will"})
+store.dispatch({type: "CHANGE_AGE", payload: 35})
+store.dispatch({type: "CHANGE_AGE", payload: 36})
