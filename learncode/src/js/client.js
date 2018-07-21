5a7b8ecff482a3f052dd200045796b64cbde7d00
Unknown <juasmartinezbel@unal.edu.co>
2018-07-20 22:27:40 -0500

    Acciones ahora usan promesas

--- learncode/src/js/client.js
+++ learncode/src/js/client.js
@@ -5,7 +5,7 @@
 	import {createLogger} from "redux-logger"; //npm install -S "redux-logger"
 	import thunk from "redux-thunk"; //npm install -S "redux-thunk"
 	import axios from "axios"; //npm install -S "axios"
-
+	import promise from "redux-promise-middleware"
 const initialState={
 	fetching: false,
 	fetched: false,
@@ -15,15 +15,15 @@ const initialState={
 
 const reducer = function(state=initialState, action){
 	switch(action.type){
-		case "FETCH_USER_START":{
+		case "FETCH_USER_PENDING":{
 			return {...state, fetching: true}
 			break;
 		}
-		case "FETCH_USER_ERROR":{
+		case "FETCH_USER_REJECTED":{
 			return {...state, fetching: false, error: action.payload}
 			break;
 		}
-		case "RECEIVE_USERS" :{
+		case "FETCH_USER_FULFILLED" :{
 			return {...state, fetching: false, fetched: true, users: action.payload}
 			break;
 		}
@@ -32,18 +32,16 @@ const reducer = function(state=initialState, action){
 }
 
 							
-const middleware = applyMiddleware(thunk, createLogger());
+const middleware = applyMiddleware(promise(), thunk, createLogger());
 
 const store = createStore(reducer, middleware)
 
-//En vez de enviar un objeto, enviemos el despachador
-store.dispatch(dispatch =>{
-	dispatch({type: "FETCH_USER_START"})
-	axios.get("http://rest.learncode.academy/api/wstern/users").then((response)=>{ //Cambiar url pa ver error
-		dispatch({type: "RECEIVE_USERS", payload:response.data})
-	}).catch((err)=>{
-		dispatch({type:"FETCH_USER_ERROR", payload: err})
-	})
+//
+store.dispatch({
+	type:"FETCH_USER",
+	payload: axios.get("http://rest.learncode.academy/api/wstern/users") //Es un tipo promesa y enviará el tipo para guardarse
+	//Si es valido será TYPE_PENDING ---> TYPE_FULFILLED
+	//Si tiene errores será TYPE_PENDING--->TYPE_REJECTED
 })
 
 //Es un montón de funciones sincronas
