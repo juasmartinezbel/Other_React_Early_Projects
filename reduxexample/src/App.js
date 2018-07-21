3e6e7c3991f69389932fcd9fb3b7bf44d2f57701
Unknown <juasmartinezbel@unal.edu.co>
2018-07-21 17:41:05 -0500

    Fetch information from a JSON

--- reduxexample/src/App.js
+++ reduxexample/src/App.js
@@ -8,13 +8,18 @@
 //Components
   import Posts from './components/Posts'
 
+
+//Todo se tomará de aquí: https://jsonplaceholder.typicode.com/
 class App extends Component {
 
   //Cuando el componente se monte, ejecutará la acción
   componentWillMount(){
-    console.log(123);
+    //console.log(123);
+    fetch('https://jsonplaceholder.typicode.com/posts') //Hacemos fetch del origen y retorna una promesa
+    .then(res => res.json()) //El resultado res se debe pasar a un json
+    .then(data => console.log(data)) //El resultado data del anterior se pasa a los logs
   }
-  
+
   render() {
     return (
       <div className="App">
