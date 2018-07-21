0d6acd488f819b9416a7bac7b6ea07f7c1f86c62
Unknown <juasmartinezbel@unal.edu.co>
2018-07-21 18:37:40 -0500

    Ya se pueden hacer posts imaginarios desde el formulario

--- reduxexample/src/components/Postform.js
+++ reduxexample/src/components/Postform.js
@@ -10,19 +10,37 @@ class Postform extends React.Component{
 		};
 
 		this.onChange=this.onChange.bind(this);
+		this.onSubmit=this.onSubmit.bind(this);
 	}
 
 	onChange(e){
 		//Saber de qué hablamos, si de body o title
-						//title-body	//Contenido
+						//title&/or body	//Contenido
 		this.setState({[e.target.name]: e.target.value});
 	}
 
+	onSubmit(e){
+		e.preventDefault();
+
+		const post ={
+			title: this.state.title,
+			body: this.state.body
+		}
+
+		fetch('https://jsonplaceholder.typicode.com/posts', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify(post)
+		}).then(res => res.json()).then(data=>console.log(data))
+
+	}
 	render(){
 		return (
 			<div>
 				<h1>Add Post</h1>
-				<form>
+				<form onSubmit = {this.onSubmit}>
 					<div>
 						<label>Title: </label><br/>
 						<input type ="text" name="title" onChange={this.onChange} value={this.state.title /*default de constructor*/}/>
